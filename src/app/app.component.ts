f56f1ac2a7cf71fe18182f0a52ee615cd9cc4b10
Shivam Arora <shivamarora1@yahoo.com>
2024-07-16 20:03:06 +0530

    code for auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BrowserAuthError } from "@azure/msal-browser";
+import { getTokenWithScopes, getActiveAccount } from './auth';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'onedrive-angular';
+  userName: string = "";
+  errorMsg: string = "";
+
+  async onLogin() {
+    try {
+      this.errorMsg = ""
+      await getTokenWithScopes(["user.read", "Sites.Read.All",
+        "Files.Read.All",
+        "offline_access"])
+      const activeAcc = getActiveAccount()
+      if (activeAcc) {
+        this.userName = activeAcc?.username
+      } else {
+        this.errorMsg = "Unable to get active account info pls login again."
+
+      }
+    } catch (e) {
+      let errorMsg = "something went wrong pls re login..."
+      if (e instanceof BrowserAuthError) {
+        errorMsg = e.errorMessage
+      }
+      this.errorMsg = errorMsg
+      console.error(e)
+    }
+  }
 }
+
---
+++ src/app/auth.ts
@@ -0,0 +1,46 @@
+import { PublicClientApplication, Configuration, SilentRequest, BrowserCacheLocation, AccountInfo } from "@azure/msal-browser";
+import { combine } from "@pnp/core";
+import { IAuthenticateCommand } from "@pnp/picker-api/dist";
+
+const msalParams: Configuration = {
+    auth: {
+        authority: "https://login.microsoftonline.com/common",
+        clientId: "3242284e-c36d-4ac7-8bde-d02811524161",
+        redirectUri: window.location.origin
+    },
+    cache: { cacheLocation: BrowserCacheLocation.LocalStorage }
+}
+
+const app = new PublicClientApplication(msalParams);
+
+export async function getToken(command: IAuthenticateCommand): Promise<string> {
+    return getTokenWithScopes([`${combine(command.resource, ".default")}`]);
+}
+
+export async function getTokenWithScopes(scopes: string[], additionalAuthParams?: Omit<SilentRequest, "scopes">): Promise<string> {
+    await app.initialize()
+
+    let accessToken = "";
+    const authParams = { scopes, ...additionalAuthParams };
+
+    try {
+        const resp = await app.acquireTokenSilent(authParams!);
+        accessToken = resp.accessToken;
+    } catch (e) {
+        const resp = await app.loginPopup(authParams!);
+        app.setActiveAccount(resp.account);
+
+        if (resp.idToken) {
+            const resp2 = await app.acquireTokenSilent(authParams!);
+            accessToken = resp2.accessToken;
+
+        } else {
+            throw e;
+        }
+    }
+
+    return accessToken;
+}
+export function getActiveAccount(): AccountInfo | null {
+    return app.getActiveAccount()
+}

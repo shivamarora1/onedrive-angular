e589bc2fb01dca19e5df4a2e180d88b393cdd7d9
Shivam Arora <shivamarora1@yahoo.com>
2024-07-16 10:44:34 +0530

    docs folder

--- post-build.js
+++ post-build.js
@@ -1,4 +1,4 @@
 const fs = require('fs-extra');
-fs.remove('docs')
-fs.move('dist/onedrive-angular/browser', 'docs', (err) => { if(err) { return console.error(err); } });
-fs.remove('dist')
+// fs.remove('docs')
+// fs.move('dist/onedrive-angular/browser', 'docs', (err) => { if(err) { return console.error(err); } });
+// fs.remove('dist')

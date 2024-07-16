910322edb72d04061e57d67ab2cc54e7966a46c3
Shivam Arora <shivamarora1@yahoo.com>
2024-07-16 11:13:02 +0530

    post script change

--- post-build.js
+++ post-build.js
@@ -1,4 +1,26 @@
-const fs = require('fs-extra');
-// fs.remove('docs')
-// fs.move('dist/onedrive-angular/browser', 'docs', (err) => { if(err) { return console.error(err); } });
-// fs.remove('dist')
+const fs = require('fs');
+const path = require('path');
+
+// Define the paths to the child folder and the parent folder
+const childFolder = path.join(__dirname, 'docs/browser');
+const parentFolder = path.join(__dirname, 'docs');
+
+// Function to move all files from child folder to parent folder
+async function moveAllFiles() {
+    try {
+        const files = await fs.promises.readdir(childFolder);
+
+        for (const file of files) {
+            const currentPath = path.join(childFolder, file);
+            const destinationPath = path.join(parentFolder, file);
+
+            await fs.promises.rename(currentPath, destinationPath);
+            console.log(`${file} was moved to ${parentFolder}`);
+        }
+    } catch (err) {
+        console.error('Error moving files:', err);
+    }
+}
+
+// Run the function
+moveAllFiles();

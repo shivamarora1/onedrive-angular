c2f51447cd559211b7b70a2ad67b3edd29479e1a
Shivam Arora <shivamarora1@yahoo.com>
2024-07-20 22:38:53 +0530

    style pushed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,51 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BrowserAuthError } from "@azure/msal-browser";
+import { BrowserAuthError } from '@azure/msal-browser';
 import { getTokenWithScopes, getActiveAccount } from './auth';
 import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule,ButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'onedrive-angular';
-  userName: string = "";
-  errorMsg: string = "";
+  userName: string = '';
+  errorMsg: string = '';
+
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
 
   async onLogin() {
     try {
-      this.errorMsg = ""
-      await getTokenWithScopes(["user.read", "Sites.Read.All",
-        "Files.Read.All",
-        "offline_access"])
-      const activeAcc = getActiveAccount()
+      this.errorMsg = '';
+      await getTokenWithScopes([
+        'user.read',
+        'Sites.Read.All',
+        'Files.Read.All',
+        'offline_access',
+      ]);
+      const activeAcc = getActiveAccount();
       if (activeAcc) {
-        this.userName = activeAcc?.username
+        this.userName = activeAcc?.username;
       } else {
-        this.errorMsg = "Unable to get active account info pls login again."
-
+        this.errorMsg = 'Unable to get active account info pls login again.';
       }
     } catch (e) {
-      let errorMsg = "something went wrong pls re login..."
+      let errorMsg = 'something went wrong pls re login...';
       if (e instanceof BrowserAuthError) {
-        errorMsg = e.errorMessage
+        errorMsg = e.errorMessage;
       }
-      this.errorMsg = errorMsg
-      console.error(e)
+      this.errorMsg = errorMsg;
+      console.error(e);
     }
   }
 }
-
